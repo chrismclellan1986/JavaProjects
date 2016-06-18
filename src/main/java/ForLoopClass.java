f6a613eaafc283ecd4139ced0172f67ee837abdb
Christopher McLellan <cmclellan@live.co.uk>
2016-06-18 17:45:23 +0100

    Addition of For Loop Class

---
+++ src/main/java/ForLoopClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by cmcle_000 on 18/06/2016.
+ */
+public class ForLoopClass
+{
+    public static void main (String[] args)
+    {
+        for(int i = 0; i < 10; i ++)
+        {
+            System.out.println("Hello World " + i);
+        }
+    }
+
+}
